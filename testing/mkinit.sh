3e6ef29725c8832ea6ffca761aa0ace7f90d1e60
ibarryuk <58122967+ibarryuk@users.noreply.github.com>
2019-12-22 13:41:59 +0000

    Update mkinit.sh

--- testing/mkinit.sh
+++ testing/mkinit.sh
@@ -1,2 +1,2 @@
-sed  -i.bak -e 's/Modules=(*/Modules=(ext4/' \
+sed  -i.bak -e 's/MODULES=(*/MODULES=(ext4/' \
  -e 's/#* *HOOKS=(base udev autodetect modconf block */HOOKS=(base udev autodetect modconf block encrypt lvm2/' /etc/mkinitcpio.conf
