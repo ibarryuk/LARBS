98afdc2de1df29e18b46d98a94a66f10424cdec7
ibarryuk <58122967+ibarryuk@users.noreply.github.com>
2019-12-22 13:10:49 +0000

    Create part.sh

---
+++ part.sh
@@ -0,0 +1,19 @@
+(
+echo g # Create a new empty GPT partition table
+echo n # Add a new partition
+echo p # Primary partition
+echo 1 # Partition number
+echo   # First sector (Accept default: 1)
+echo +100M  # Last sector 
+echo n # Add a new partition
+echo p # Primary partition
+echo 1 # Partition number
+echo   # First sector (Accept default: 2)
+echo +250M  # Last sector
+echo n # Add a new partition
+echo p # Primary partition
+echo 1 # Partition number
+echo   # First sector (Accept default: 3)
+echo   # Last sector: remaining
+echo w # Write changes
+) | sudo fdisk
