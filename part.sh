9894816cfeb94e8d0c28eece90f8ac0f0204b8b0
ibarryuk <58122967+ibarryuk@users.noreply.github.com>
2019-12-22 13:17:46 +0000

    Update part.sh

--- part.sh
+++ part.sh
@@ -1,19 +1,18 @@
 (
 echo g # Create a new empty GPT partition table
 echo n # Add a new partition
-echo p # Primary partition
 echo 1 # Partition number
 echo   # First sector (Accept default: 1)
 echo +100M  # Last sector 
 echo n # Add a new partition
-echo p # Primary partition
-echo 1 # Partition number
+echo 2 # Partition number
 echo   # First sector (Accept default: 2)
 echo +250M  # Last sector
 echo n # Add a new partition
-echo p # Primary partition
-echo 1 # Partition number
+echo 3 # Partition number
 echo   # First sector (Accept default: 3)
 echo   # Last sector: remaining
 echo w # Write changes
-) | sudo fdisk
+) | sudo fdisk /dev/sda
+
+
